0bef639210d77bbccada068d7a75cd63b5563e02
Nirvaan Kohli <nirvaan.kohli@gmail.com>
2025-10-27 18:33:12 -0400

    Options put in

--- youtube/experimenting/youtube.py
+++ youtube/experimenting/youtube.py
@@ -1,3 +1,13 @@
 from yt_dlp import YoutubeDL
 
 # Options
+
+ydl_opts = {
+    "outtmpl": "%(uploader)s - %(title)s.%(ext)s",
+    "format": "bv*+ba/b",         # best video+audio
+    "merge_output_format": "mp4", # container if needed
+    "writesubtitles": True,
+    "writeautomaticsub": True,
+    "subtitleslangs": ["en"],
+    "quiet": True,
+}
